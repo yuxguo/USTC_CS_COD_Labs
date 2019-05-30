343af29fabcee8bbc67a1ff21efaba4ed17da469
YuxGuo <gyx_20170818@mail.ustc.edu.cn>
2019-05-30 19:01:50 +0800

    rm lab6

--- lab6/coe_generate/coe_generate.py
+++
@@ -1,9 +0,0 @@
-import random
-
-with open('coe.txt', 'w') as f:
-    for x in range(2**11):
-        s = (hex(random.randint(0,15)).split('x')[1]).zfill(8)
-        print (s)
-        f.write(s+'\n')
-
-
